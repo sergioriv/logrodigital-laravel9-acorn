4e626c7a30bc24c08cbcd85bb793bc8f3c96d65c
sergioriv <sergioa.rivcif@gmail.com>
2023-01-24 19:24:38 -0500

    new data student carnet pdf

--- resources/views/logro/pdf/student-carnet.blade.php
+++ resources/views/logro/pdf/student-carnet.blade.php
@@ -88,62 +88,117 @@
         .f-size-5 {
             font-size: 5px;
         }
+
+        .f-size-7 {
+            font-size: 7px;
+        }
+
         .f-size-9 {
             font-size: 9px;
         }
+
         .f-size-11 {
             font-size: 11px;
+        }
+        .p-8 {
+            padding: 8mm;
+        }
+        .line {
+            border-top: 0.3px solid #373435;
+        }
+        .table {
+            border-collapse: collapse;
+            border: 0px;
         }
     </style>
 </head>
 
 <body>
 
-    <div class="card carnet">
-        <div class="t-center vertical-center">
-            <table class="table w-100">
-                <tr>
-                    <td class="left-content">
-                        <div class="photo-student">
-                            @if (!is_null($student->user->avatar))
-                            <img src="{{ config('app.url') .'/'. $student->user->avatar }}">
-                            @endif
-                        </div>
-                    </td>
-                    <td class="right-content t-center">
-                        <div class="badge w-100">
-                            @if ($SCHOOL->badge)
-                                <img src="{{ config('app.url') . '/' . $SCHOOL->badge }}">
-                            @endif
-                        </div>
-                        <div class="bold">{{ $SCHOOL->name }}</div>
-                        <div class="bold mt-2 f-size-9 text-uppercase">
-                            {{ $student->getNames() }}
-                        </div>
-                        <div class="text-uppercase">
-                            {{ $student->getLastNames() }}
-                        </div>
-                        <div class="text-uppercase">
-                            {{ $student->document_type_code .' - '. $student->document }}
-                        </div>
-                        <div class="text-uppercase mt-2 f-size-11 bold">
-                            {{ $student->group->name }}
-                        </div>
-                        <div class="text-uppercase bold f-size-5">
-                            {{ $student->headquarters->name }}
-                            |
-                            {{ $student->studyTime->name }}
-                        </div>
+    <table class="table" border="0">
+        <tr>
+            <td>
+                <div class="card carnet">
+                    <div class="t-center">
+                        <table class="table w-100">
+                            <tr>
+                                <td class="left-content">
+                                    <div class="photo-student">
+                                        @if (!is_null($student->user->avatar))
+                                            <img src="{{ config('app.url') . '/' . $student->user->avatar }}">
+                                        @endif
+                                    </div>
+                                </td>
+                                <td class="right-content t-center">
+                                    <div class="badge w-100">
+                                        @if ($SCHOOL->badge)
+                                            <img src="{{ config('app.url') . '/' . $SCHOOL->badge }}">
+                                        @endif
+                                    </div>
+                                    <div class="bold text-uppercase f-size-5">{{ $SCHOOL->name }}</div>
+                                    <div class="bold mt-2 f-size-9 text-uppercase">
+                                        {{ $student->getNames() }}
+                                    </div>
+                                    <div class="text-uppercase">
+                                        {{ $student->getLastNames() }}
+                                    </div>
+                                    <div class="text-uppercase">
+                                        {{ $student->document_type_code . ' - ' . $student->document }}
+                                    </div>
+                                    <div class="text-uppercase mt-2 f-size-11 bold">
+                                        {{ $student->group->name }}
+                                    </div>
+                                    <div class="text-uppercase bold f-size-5">
+                                        {{ $student->headquarters->name }}
+                                        |
+                                        {{ $student->studyTime->name }}
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div class="card carnet">
+                    <div class="t-center p-8">
+
+                        <div class="text-uppercase bold f-size-11">ESTUDIANTE</div>
+
+                        <div>&nbsp;</div>
+                        <div>&nbsp;</div>
+
+                        <div class="f-size-5">
+                            Este documento es personal e intransferible.
+                            Este documento lo identifica como estudiante de la Institución <b class="text-uppercase f-size-5">{{ $SCHOOL->name }}</b>.
+                            Su titular se obliga a darle el debido uso y guardar comportamiento ético y cívico referidos a la comunidad educativa y a la sociedad en general.</div>
+
+                        <div class="mt-2 f-size-5 bold">Válido durante el año lectivo {{ now()->format('Y') }}</div>
+
+                        <div>&nbsp;</div>
+                        <div>&nbsp;</div>
+                        <div>&nbsp;</div>
+
+                        <div class="line"></div>
+
                         <div class="mt-2 f-size-5">
                             Tel. {{ $SCHOOL->contact_telephone }}
                             <br />
                             Carrera 22 No. 4 - 44 Sur Barrio El Sociego - Madrid - Cundinamarca
                         </div>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    </div>
+
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+
+
+
+
+    </section>
+
 
 </body>
 
