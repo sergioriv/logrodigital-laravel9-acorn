e8a951cafc0410ae0d0e3e5a963ece704bd5d2f2
sergioriv <sergioa.rivcif@gmail.com>
2023-01-26 14:00:24 -0500

    fix loader.js

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -36,4 +36,3 @@
 <link rel="stylesheet" href="/css/styles.css" />
 <link rel="stylesheet" href="/css/main.css?d=1668555514494" />
 <!-- Template Base Styles End -->
-<script src="/js/base/loader.js"></script>
