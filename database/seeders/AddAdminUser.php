1ed7e88ea120b1462237ef2a29a2c89dfd02cf02
sergioriv <sergioa.rivcif@gmail.com>
2023-09-29 15:46:45 -0500

    new seeder create admin

--- database/seeders/AddAdminUser.php
+++ database/seeders/AddAdminUser.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class AddAdminUser extends Seeder
 {
@@ -22,6 +23,8 @@ class AddAdminUser extends Seeder
             'active' => TRUE,
         ]);
 
-        $user->assignRole(1);
+        $SUPPORT = Role::where('name', 'SUPPORT')->first();
+
+        $user->assignRole($SUPPORT->id);
     }
 }
