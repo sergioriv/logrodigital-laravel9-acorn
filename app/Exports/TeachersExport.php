fe1e70363bf133ebdb1caf0e6a58f67c89dedd3b
sergioriv <sergioa.rivcif@gmail.com>
2024-06-11 10:46:00 -0500

    add data teacher export

--- app/Exports/TeachersExport.php
+++ app/Exports/TeachersExport.php
@@ -3,42 +3,54 @@
 namespace App\Exports;
 
 use App\Models\Teacher;
-use App\Models\User;
-use Illuminate\Support\Facades\Date;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithColumnWidths;
-use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class TeachersExport implements FromCollection, WithHeadings, WithColumnWidths, WithStyles
+class TeachersExport implements FromArray, ShouldAutoSize, WithStyles
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    public function array(): array
     {
-        return Teacher::select("names", "last_names", "telephone", "institutional_email", "document")->get();
-    }
-
-    public function headings(): array
-    {
-        return ["Nombres", "Apellidos", "Telefono", "Email", "Documento"];
-    }
-
-    public function columnWidths(): array
-    {
-        return [
-            'A' => 20,
-            'B' => 30,
-            'C' => 20,
-            'D' => 20,
-            'E' => 20,
-            'F' => 20,
-            'G' => 20,
-            'H' => 20,
+        $array = [
+            ["Nombres", "Apellidos", "Telefono", "Celular", "Correo institucional", "Ciudad de expedición", "No. documento", "Fecha nacimiento", "Ciudad residencia", "Dirección", "Estado civil", "Fecha de ingreso"]
         ];
+
+        $teachers = Teacher::select(
+            "names",
+            "last_names",
+            "telephone",
+            "cellphone",
+            "institutional_email",
+            "expedition_city",
+            "document",
+            "birthdate",
+            "residence_city",
+            "address",
+            "marital_status",
+            "date_entry")
+            ->with('expeditionCity', 'birthCity', 'residenceCity')->get();
+
+        foreach ($teachers as $teacher) {
+
+            $array[] = [
+                $teacher->names,
+                $teacher->last_names,
+                $teacher->telephone,
+                $teacher->cellphone,
+                $teacher->institutional_email,
+                $teacher->expeditionCity?->name,
+                $teacher->document,
+                $teacher->birthdate,
+                $teacher->residenceCity?->name,
+                $teacher->address,
+                is_null($teacher->marital_status) ? null : __($teacher->marital_status),
+                $teacher->date_entry
+            ];
+
+        }
+
+        return $array;
     }
 
     public function styles(Worksheet $sheet)
